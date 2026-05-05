175293c2f6fe49bf4597c50b0f8d3fb2824ee482
Filip Gulde <f.gulde@outlook.de>
2026-05-06 00:09:02 +0200

    Add Read-Print-Eval Loop

--- src/main.c
+++ src/main.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 
 int main() {
-  printf("$ ");
-  fflush(stdout);
-
-  // Wait for user input
   char input[256];
-  fgets(input, 256, stdin);
+
+  for (;;) {
+    printf("$ ");
+    fflush(stdout);
+
+    if (fgets(input, sizeof(input), stdin) == NULL) {
+      break;
+    }
+
+    //
+    char* command_name = input;
+    while (*command_name == ' ' || *command_name == '\t') {
+      command_name++;
+    }
+
+    char* end = command_name;
+    while (*end != '\0' && *end != ' ' && *end != '\t' && *end != '\n') {
+      end++;
+    }
+    *end = '\0';
+
+    if (*command_name != '\0') {
+      printf("%s: command not found\n", command_name);
+    }
+  }
 
   return 0;
 }
